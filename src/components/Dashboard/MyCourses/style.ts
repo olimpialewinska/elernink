309b588fa663e8c7540f4895b8f062bba26c575a
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-04-06 10:56:58 +0200

    dashboard

--- src/components/Dashboard/MyCourses/style.ts
+++ src/components/Dashboard/MyCourses/style.ts
@@ -66,9 +66,13 @@ export const Text = styled.div`
 
 export const Statistics = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 10px;
   padding: 20px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const Box = styled.div`
