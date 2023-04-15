ba33bfa35f0680f7442a5d5c1b5f410af0d2c0a7
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-04-15 13:35:32 +0200

    create course design

--- src/components/Dashboard/Create/index.tsx
+++ src/components/Dashboard/Create/index.tsx
@@ -1,28 +1,107 @@
+import { DragEvent, useCallback, useRef, useState } from "react";
 import {
+  Box,
   Container,
+  Content,
   CreateButton,
+  DeleteButton,
   DescriptionInput,
+  Divider,
   IdentityBox,
   ImageInput,
+  LessonContent,
   NameInput,
   Navbar,
+  NewTopicButton,
   Title,
+  Topic,
+  TopicTitle,
+  Wrapper,
+  CodeInput,
 } from "./style";
 
+interface Topic {
+  name: string;
+  lesson: string;
+}
+
 export function Create() {
+  const dragItem = useRef<number | null>(0);
+  const dragOverItem = useRef<number | null>();
+  const [list, setList] = useState<Topic[]>([
+    { name: "Topic", lesson: "Lesson" },
+  ]);
+
+  const dragStart = useCallback(
+    (e: DragEvent<HTMLDivElement>, position: number) => {
+      dragItem.current = position;
+    },
+    []
+  );
+
+  const dragEnter = useCallback(
+    (e: DragEvent<HTMLDivElement>, position: number | null | undefined) => {
+      dragOverItem.current = position;
+    },
+    []
+  );
+
+  const drop = useCallback(() => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current!];
+    copyListItems.splice(dragItem.current!, 1);
+    copyListItems.splice(dragOverItem.current!, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  }, [list]);
+
+  const addNewItem = useCallback(() => {
+    setList((prev) => [...prev, { name: `Topic`, lesson: `Lesson` }]);
+  }, []);
+
+  const deleteItem = useCallback((index: number) => {
+    setList((prev) => prev.filter((_, i) => i !== index));
+  }, []);
+
   return (
     <>
       <Navbar>
         <Title>Create</Title>
+        <CreateButton draggable>Create</CreateButton>
       </Navbar>
       <Container>
-        <IdentityBox>
-          <NameInput placeholder="Name" required />
-          <ImageInput type="file" />
-          <DescriptionInput placeholder="Description" />
-        </IdentityBox>
+        <Box>
+          <IdentityBox>
+            <Wrapper>
+              <NameInput placeholder="Name" required />
+              <CodeInput placeholder="Course code" required />
+            </Wrapper>
 
-        <CreateButton>Create</CreateButton>
+            <ImageInput type="file" />
+            <DescriptionInput placeholder="Description" />
+          </IdentityBox>
+          {list &&
+            list.map((item, index) => (
+              <>
+                <Topic
+                  onDragStart={(e) => dragStart(e, index)}
+                  onDragEnter={(e) => dragEnter(e, index)}
+                  onDragEnd={drop}
+                  key={index}
+                  draggable
+                >
+                  <TopicTitle value={item.name} />
+                  <DeleteButton onClick={() => deleteItem(index)} />
+                </Topic>
+                <Content>
+                  <LessonContent placeholder="Lesson" value={item.lesson} />
+                  <Divider />
+                </Content>
+              </>
+            ))}
+          <NewTopicButton onClick={addNewItem}>+</NewTopicButton>
+        </Box>
       </Container>
     </>
   );
--- src/components/Dashboard/Create/style.ts
+++ src/components/Dashboard/Create/style.ts
@@ -19,14 +19,37 @@ export const Title = styled.div`
 `;
 
 export const Container = styled.div`
-  height: calc(100% - 150px);
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  overflow-y: auto;
 `;
 
 export const NameInput = styled.input`
   width: 400px;
+  border: none;
+  font-size: 16px;
+  padding: 16px;
+  outline: none;
+  border-radius: 30px;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  padding: 0 20px;
+  border-radius: 8px;
+  transition: 0.5s;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+
+  @media (max-width: 768px) {
+    width: 90%;
+    margin-bottom: 20px;
+  }
+`;
+export const CodeInput = styled.input`
+  width: 200px;
   border: none;
   font-size: 16px;
   padding: 16px;
@@ -62,6 +85,8 @@ export const DescriptionInput = styled.textarea`
   border-radius: 8px;
   transition: 0.5s;
   border: 1px solid rgba(0, 0, 0, 0.1);
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  resize: vertical;
 
   @media (max-width: 768px) {
     width: 90%;
@@ -131,8 +156,165 @@ export const IdentityBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  margin-bottom: 20px;
 
   @media (max-width: 768px) {
     align-items: center;
+  }
+`;
+
+export const Topic = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 30px;
+  margin: 10px 0;
+  background-color: rgba(0, 0, 0, 0.08);
+  border-radius: 10px;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.05);
+  z-index: 30;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+export const TopicTitle = styled.input`
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #333;
+  font-size: 18px;
+  font-weight: 600;
+  color: #333;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 30px;
+  margin-top: -14px;
+  margin-bottom: 20px;
+  background-color: #fff;
+  border-radius: 0 0 10px 10px;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);
+  transition: height 0.3s;
+
+  z-index: 2;
+
+  @media (max-width: 768px) {
+    padding: 20px;
+    width: 90%;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: rgba(0, 0, 0, 0.1);
+  margin: 20px 0;
+`;
+
+export const LessonContent = styled.textarea`
+  width: 100%;
+  border: none;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #333;
+  resize: vertical;
+  color: #333;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.5);
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  }
+`;
+
+export const Box = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  align-items: center;
+`;
+
+export const NewTopicButton = styled.div`
+  width: 50px;
+  height: 50px;
+  background-image: linear-gradient(
+    0deg,
+    rgba(133, 183, 219, 0.3) 0%,
+    rgba(205, 170, 233, 0.3) 100%
+  );
+
+  border: none;
+  outline: none;
+  border-radius: 50%;
+  margin-bottom: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  text-align: center;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:after {
+    font-size: 40px;
+    content: "";
+    transition: 0.2s opacity;
+    display: block;
+    inset: 0;
+    position: absolute;
+    background-image: linear-gradient(
+      0deg,
+      rgba(133, 183, 219, 0.4) 0%,
+      rgba(205, 170, 233, 0.4) 100%
+    );
+    opacity: 0;
+  }
+
+  &:hover {
+    &:after {
+      opacity: 1;
+    }
+  }
+`;
+
+export const DeleteButton = styled.div`
+  width: 20px;
+  height: 20px;
+  background-image: url("/delete.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 0.3s;
+  cursor: pointer;
+  margin-left: 14px;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
   }
 `;
